35ba758d9da5ddf1d4bd88a89039ee660435d59e
sibi <sibikottukandiyil2@gmail.com>
2023-11-11 12:30:48 +0530

    Now username is using instead of email for login. logout redirect to login page instead of home page (maybe change needed )

--- accounts/views.py
+++ accounts/views.py
@@ -8,25 +8,17 @@ from django.contrib.auth import logout
 
 def login(request):
     if request.method == 'POST':
-        print("Reached\n\n\n")
-        email = request.POST['email']
+        username = request.POST['username']
         password = request.POST['password']
 
-        # print("Email : ",email)
-        # print("Password : ",password)
-
-        user = authenticate(username=email,password = password)
+        user = authenticate(username = username,password = password)
 
         if user is not None:
-            print("LOGIN\n\n")
             auth.login(request,user)
             return redirect('/')
-        else:
-            print("Error!!!!!")
-
 
     return render(request,'login.html')
 
 def logout_view(request):
         logout(request)
-        return redirect('/')
+        return redirect('login')
